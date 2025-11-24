61ca7ba12e3b8b5ec72376d1d1b311b3da6684c3
michael-disalvo <mtdisavl3@gmail.com>
2025-11-24 17:13:46 -0500

    added parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 use std::iter::Peekable;
-use std::ops::{Deref, DerefMut};
-use std::str::Chars;
+
+struct IntoChars {
+    string: String,
+    pos: usize,
+}
+
+impl IntoChars {
+    fn new(string: String) -> Self {
+        let string = string.to_string();
+        Self { string, pos: 0 }
+    }
+}
+
+impl std::iter::Iterator for IntoChars {
+    type Item = char;
+    fn next(&mut self) -> Option<Self::Item> {
+        let c = self.string[self.pos..].chars().next()?;
+        self.pos += c.len_utf8();
+        Some(c)
+    }
+}
 
 #[repr(isize)]
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Token {
     EOF = -1,
     NEWLINE = 0,
@@ -37,6 +56,13 @@ pub enum Token {
 }
 
 impl Token {
+    fn is_comparator(&self) -> bool {
+        matches!(
+            self,
+            Token::EQEQ | Token::NOTEQ | Token::LT | Token::LTEQ | Token::GT | Token::GTEQ
+        )
+    }
+
     fn is_eof(&self) -> bool {
         matches!(self, Token::EOF)
     }
@@ -60,6 +86,148 @@ impl Token {
 
     fn try_keyword_or_ident(s: String) -> Self {
         Token::try_keyword(&s).unwrap_or(Token::IDENT(s))
+    }
+}
+
+fn newline_optional(lexer: &mut Lexer) {
+    while *lexer.peek_token() == Token::NEWLINE {
+        let _ = lexer.next_token();
+    }
+}
+
+fn newline(lexer: &mut Lexer) {
+    println!("NEWLINE");
+    match lexer.next_token() {
+        Token::NEWLINE => newline_optional(lexer),
+        other => panic!("Parse error. Expected newline but found {:?}", other),
+    }
+}
+
+fn primary(lexer: &mut Lexer) {
+    println!("PRIMARY");
+    match lexer.next_token() {
+        Token::NUMBER(..) => {}
+        Token::IDENT(..) => {}
+        other => panic!(
+            "Parse error. Expected number or identifier but found {:?}",
+            other
+        ),
+    }
+}
+
+fn unary(lexer: &mut Lexer) {
+    println!("UNARY");
+    match lexer.peek_token() {
+        Token::PLUS | Token::MINUS => {
+            let _ = lexer.next_token();
+        }
+        _ => {}
+    }
+    primary(lexer)
+}
+
+fn term(lexer: &mut Lexer) {
+    println!("TERM");
+
+    unary(lexer);
+
+    while matches!(lexer.peek_token(), Token::SLASH | Token::ASTERISK) {
+        lexer.next_token();
+        unary(lexer);
+    }
+}
+
+fn expression(lexer: &mut Lexer) {
+    println!("EXPRESSION");
+
+    term(lexer);
+    while matches!(lexer.peek_token(), Token::PLUS | Token::MINUS) {
+        lexer.next_token();
+        term(lexer);
+    }
+}
+
+fn comparison(lexer: &mut Lexer) {
+    println!("COMPARISON");
+    expression(lexer);
+
+    if !lexer.peek_token().is_comparator() {
+        panic!(
+            "Parse error. Expected comparator, found {:?}",
+            lexer.peek_token()
+        );
+    }
+
+    while lexer.peek_token().is_comparator() {
+        lexer.next_token();
+        expression(lexer);
+    }
+}
+
+fn statement(lexer: &mut Lexer) {
+    println!("STATEMENT");
+    match lexer.next_token() {
+        Token::EOF => unreachable!(),
+        Token::PRINT => match lexer.peek_token() {
+            &Token::STRING(..) => {
+                let _ = lexer.next_token();
+            }
+            _ => expression(lexer),
+        },
+        Token::IF => {
+            comparison(lexer);
+            match lexer.next_token() {
+                Token::THEN => {}
+                other => panic!("Parser error. Expected THEN but found {:?}", other),
+            }
+
+            newline(lexer);
+            while *lexer.peek_token() != Token::ENDIF {
+                statement(lexer);
+            }
+            lexer.next_token();
+        }
+        Token::WHILE => {
+            comparison(lexer);
+            match lexer.next_token() {
+                Token::REPEAT => {}
+                other => panic!("Parser error. Expected REPEAT but found {:?}", other),
+            }
+            newline(lexer);
+            while *lexer.peek_token() != Token::ENDWHILE {
+                statement(lexer);
+            }
+            lexer.next_token();
+        }
+        Token::LABEL | Token::GOTO | Token::INPUT => match lexer.next_token() {
+            Token::IDENT(..) => {}
+            other => panic!("Parser error. Expected identifier but found {:?}", other),
+        },
+        Token::LET => {
+            match lexer.next_token() {
+                Token::IDENT(..) => {}
+                other => panic!("Parser error. Expected identifier but found {:?}", other),
+            }
+            match lexer.next_token() {
+                Token::EQ => {}
+                other => panic!("Parser error. Expected `=` but found {:?}", other),
+            }
+            expression(lexer);
+        }
+        other => panic!(
+            "Parser error. Expected start of statement but found {:?}",
+            other
+        ),
+    }
+
+    newline(lexer);
+}
+
+fn program(mut lexer: Lexer) {
+    println!("PROGRAM");
+    newline_optional(&mut lexer);
+    while !lexer.peek_token().is_eof() {
+        statement(&mut lexer)
     }
 }
 
@@ -99,13 +267,18 @@ impl StartOperator {
     }
 }
 
-pub struct TokenIter<'s> {
-    lexer: Lexer<'s>,
+fn is_operator(ch: char) -> bool {
+    matches!(ch, '+' | '-' | '*' | '/' | '=' | '>' | '<')
+}
+
+pub struct TokenIter {
+    lexer: Lexer,
     seen_eof: bool,
 }
 
-impl<'s> std::iter::Iterator for TokenIter<'s> {
+impl std::iter::Iterator for TokenIter {
     type Item = Token;
+
     fn next(&mut self) -> Option<Self::Item> {
         if self.seen_eof {
             None
@@ -117,40 +290,58 @@ impl<'s> std::iter::Iterator for TokenIter<'s> {
     }
 }
 
-fn is_operator(ch: char) -> bool {
-    matches!(ch, '+' | '-' | '*' | '/' | '=' | '>' | '<')
+pub struct Lexer {
+    inner: Peekable<IntoChars>,
+    peeked: Option<Token>,
 }
 
-pub struct Lexer<'s>(Peekable<Chars<'s>>);
-
-impl<'s> Lexer<'s> {
-    pub fn new(s: &'s str) -> Lexer<'s> {
-        Lexer(s.chars().peekable())
+impl Lexer {
+    pub fn new(s: &str) -> Lexer {
+        let s = s.to_string() + "\n";
+        Lexer {
+            inner: IntoChars::new(s).peekable(),
+            peeked: None,
+        }
     }
-    pub fn tokens(self) -> TokenIter<'s> {
+
+    fn skip_whitespace(&mut self) {
+        while matches!(self.inner.peek(), Some(' ') | Some('\t') | Some('\r')) {
+            self.inner.next();
+        }
+    }
+
+    fn peek_char(&mut self) -> Option<char> {
+        self.inner.peek().copied()
+    }
+
+    fn next_char(&mut self) -> Option<char> {
+        self.inner.next()
+    }
+
+    pub fn tokens(self) -> TokenIter {
         TokenIter {
             lexer: self,
             seen_eof: false,
         }
     }
 
-    fn skip_whitespace(&mut self) {
-        while matches!(self.0.peek(), Some(' ') | Some('\t') | Some('\r')) {
-            self.0.next();
+    pub fn peek_token(&mut self) -> &Token {
+        if let None = self.peeked {
+            self.peeked = Some(self.next_token())
         }
-    }
 
-    fn peek(&mut self) -> Option<char> {
-        self.0.peek().copied()
-    }
-
-    fn next_char(&mut self) -> Option<char> {
-        self.0.next()
+        // SAFETY: a `None` variant for `self` would have been replaced by a `Some`
+        // variant in the code above.
+        unsafe { self.peeked.as_mut().unwrap_unchecked() }
     }
 
     // all remaining tokens are basically strings of text, delimited by newlines or spaces, that
     // need to be categorized
     pub fn next_token(&mut self) -> Token {
+        if let Some(v) = self.peeked.take() {
+            return v;
+        }
+
         let mut token_string = String::new();
         let mut state = State::Started;
 
@@ -165,9 +356,7 @@ impl<'s> Lexer<'s> {
                             token_string.push(ch);
                             match ch {
                                 '+' => State::Finished(Token::PLUS),
-                                '-' if !self.peek().is_some_and(|ch| ch.is_digit(10)) => {
-                                    State::Finished(Token::MINUS)
-                                }
+                                '-' => State::Finished(Token::MINUS),
                                 '*' => State::Finished(Token::ASTERISK),
                                 '/' => State::Finished(Token::SLASH),
                                 '\n' => State::Finished(Token::NEWLINE),
@@ -175,9 +364,7 @@ impl<'s> Lexer<'s> {
                                 '>' => State::InOperator(StartOperator::GT),
                                 '<' => State::InOperator(StartOperator::LT),
                                 '"' => State::InString,
-                                other if other == '-' || other.is_digit(10) => {
-                                    State::InNumeric(false)
-                                }
+                                other if other.is_digit(10) => State::InNumeric(false),
                                 other if other.is_alphabetic() => State::InAlpha,
                                 other => panic!("Lexer error. Unknown start to token: {}", other),
                             }
@@ -194,7 +381,7 @@ impl<'s> Lexer<'s> {
                         }
                     }
                 },
-                State::InOperator(start_operator) => match self.peek() {
+                State::InOperator(start_operator) => match self.peek_char() {
                     None => State::Finished(start_operator.into_token()),
                     Some(ch) => match ch {
                         '=' => {
@@ -205,7 +392,7 @@ impl<'s> Lexer<'s> {
                         _ => State::Finished(start_operator.into_token()),
                     },
                 },
-                State::InNumeric(seen_period) => match self.peek() {
+                State::InNumeric(seen_period) => match self.peek_char() {
                     None => State::Finished(Token::NUMBER(token_string.clone())),
                     Some(ch) => match ch {
                         '.' => {
@@ -235,7 +422,7 @@ impl<'s> Lexer<'s> {
                         }
                     },
                 },
-                State::InAlpha => match self.peek() {
+                State::InAlpha => match self.peek_char() {
                     None => State::Finished(Token::try_keyword_or_ident(token_string.clone())),
                     Some(ch) => match ch {
                         '_' => {
@@ -265,47 +452,68 @@ impl<'s> Lexer<'s> {
     }
 }
 
-impl<'s> Deref for Lexer<'s> {
-    type Target = Peekable<Chars<'s>>;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl<'s> DerefMut for Lexer<'s> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
 fn main() {
     let s = r#"
-LET X = 442
-LET Y = 32.12
-LET Z = X + Y
-PRINT Z
-LET MY_STRING = "WOW"
-PRINT MY_STRING 
-IF Z > 5 THEN
-    PRINT "WORKED"
-ENDIF
-LET NEG_X = -4.312
-        "#;
+LET X = 3
+PRINT X
+IF X >= 2 THEN
+    LET Y = 5
+    WHILE Y*2 > 3 REPEAT 
+        PRINT Y
+        LET Y = Y - 1
+    ENDWHILE 
+    PRINT "LARGE"
+ENDIF"#;
 
-    let mut lexer = Lexer::new(&s);
-
-    loop {
-        let token = lexer.next_token();
-        println!("{:?}", token);
-        if token.is_eof() {
-            break;
-        }
-    }
+    let lexer = Lexer::new(&s);
+    program(lexer);
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_char_in_alpha_token() {
+        let input = r#"LET MY_FUN_👍_VAR = 3.23"#;
+        let _: Vec<_> = Lexer::new(input).tokens().collect();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_alpha_in_number() {
+        let input = r#"LET X = 3.23A"#;
+        let _: Vec<_> = Lexer::new(input).tokens().collect();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_period_start_token() {
+        let input = r#"LET X = .3"#;
+        let _: Vec<_> = Lexer::new(input).tokens().collect();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_double_period_in_number() {
+        let input = r#"LET X = 4.3.2"#;
+        let _: Vec<_> = Lexer::new(input).tokens().collect();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_missing_string_end() {
+        let input = r#"LET S = "this does not end"#;
+        let _: Vec<_> = Lexer::new(input).tokens().collect();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_unknown_token_start() {
+        let input = r#"LET 👍 = 3"#;
+        let _: Vec<_> = Lexer::new(input).tokens().collect();
+    }
 
     #[test]
     fn test_edge_case_operator_delimited_things() {
@@ -367,7 +575,8 @@ ENDIF"#,
                     LET,
                     IDENT("X".to_string()),
                     EQ,
-                    NUMBER("-4.6".to_string()),
+                    MINUS,
+                    NUMBER("4.6".to_string()),
                     NEWLINE,
                     LET,
                     IDENT("Y2".to_string()),
